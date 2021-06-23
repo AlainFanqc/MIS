6322173ef61896be733528578a2fe5ede1c76cc0
Administrator <1604825334@qq.com>
2021-06-24 01:01:39 +0800

    6.23

--- app/src/main/java/com/example/termwork/Administrator.java
+++ app/src/main/java/com/example/termwork/Administrator.java
@@ -3,17 +3,17 @@ package com.example.termwork;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
-public class Administrator extends AppCompatActivity implements Runnable{
+public class Administrator extends AppCompatActivity {
 
-    private static PreparedStatement stmt = null;
-    private static Connection con = null;
     EditText add_dishname,add_dishprice,add_introduction;
     Button add_submit;
     @Override
@@ -24,17 +24,32 @@ public class Administrator extends AppCompatActivity implements Runnable{
         add_dishprice = findViewById(R.id.add_dishprice);
         add_introduction = findViewById(R.id.add_dishintroduction);
         add_submit = findViewById(R.id.add_submit);
-        Thread thread = new Thread(this);
-        thread.start();
+        add_submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                insert();
+
+            }
+        });
     }
 
-    @Override
-    public void run() {
+    public void insert() {
         try {
-            con = MySQLConnections.getConnection();
-            String sql ="insert into canteen('dishname','price',''dishcontent) values(?,?,?)";
-            if (con!=null){
-
+            Connection con = MySQLConnections.getConnection();
+            PreparedStatement ps_0 = con.prepareStatement("select dishnum from canteen");
+            ResultSet rs = ps_0.executeQuery();
+            while (rs.last()) {
+                int x = rs.getInt(1);
+                Log.i("X", "insert:X:  "+x);
+                String sql = "insert into canteen values(?,?,?,?)";
+                PreparedStatement ps = con.prepareStatement(sql);
+                ps.setInt(1,x+1);
+                ps.setString(2, add_dishname.getText().toString());
+                ps.setString(3, add_introduction.getText().toString());
+                ps.setFloat(4, Float.valueOf(add_dishprice.getText().toString()));
+                int i = ps.executeUpdate();
+                Log.i(".....", "insert: i: " + i);
+                break;
             }
         } catch (Exception e) {
             e.printStackTrace();
